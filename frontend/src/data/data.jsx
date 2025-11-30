70e43d559239efc9e3e45e328bd3d9fcce6d9fbc
Arshad Shaik <arshadshaik2007@gmail.com>
2025-11-30 10:22:09 +0530

    Update API endpoints to use production URL

--- frontend/src/data/data.jsx
+++ frontend/src/data/data.jsx
@@ -1,5 +1,5 @@
 async function getNotes() {
-  const res = await fetch("http://localhost:8000/api/notes");
+  const res = await fetch("https://notez0.onrender.com/api/notes");
   console.log(res);
   if (!res.ok) {
     throw new Error("failed to fetch");
@@ -10,7 +10,7 @@ async function getNotes() {
 }
 
 async function getByTitle(title) {
-  const res = await fetch(`http://localhost:8000/api/notes/${title}`);
+  const res = await fetch(`https://notez0.onrender.com/api/notes/${title}`);
   if (!res.ok) throw new Error("note not found");
   return await res.json();
 }
@@ -22,7 +22,7 @@ async function deleteNote(title) {
       "Content-Type": "application/json",
     },
   };
-  let res = await fetch(`http://localhost:8000/api/notes/${title}`, options);
+  let res = await fetch(`https://notez0.onrender.com/api/notes/${title}`, options);
   res = await res.json();
   return res;
 }
@@ -35,7 +35,7 @@ async function postNote(newNote) {
     },
     body: JSON.stringify(newNote),
   };
-  const notes = await fetch(`http://localhost:8000/api/notes`, options);
+  const notes = await fetch(`https://notez0.onrender.com/api/notes`, options);
   let res = await notes.json();
   console.log(res);
   return res;
@@ -49,7 +49,7 @@ async function updateNote(title, updatedNote) {
     },
     body: JSON.stringify(updatedNote),
   };
-  const note = await fetch(`http://localhost:8000/api/notes/${title}`, options);
+  const note = await fetch(`https://notez0.onrender.com/api/notes/${title}`, options);
   let res = await note.json();
   return res;
 }
